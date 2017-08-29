47f7790dda2fea3cc08776656398a7b02e6103da
thanatat <30922223+weare1909@users.noreply.github.com>
2017-08-29 21:39:58 +0700

    เพิ่มเติม จ้า

--- CPE200Lab1/CPE200Lab1/MainForm.Designer.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.Designer.cs
@@ -51,10 +51,10 @@
             this.lblDisplay = new System.Windows.Forms.Label();
             this.btn1Dividex = new System.Windows.Forms.Button();
             this.btnSqrt = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnMC = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnMS = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn7
@@ -310,15 +310,15 @@
             this.btnSqrt.Text = "SQRT";
             this.btnSqrt.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnMC
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.button1.Location = new System.Drawing.Point(388, 218);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 64);
-            this.button1.TabIndex = 23;
-            this.button1.Text = "M+";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnMC.Location = new System.Drawing.Point(388, 218);
+            this.btnMC.Name = "btnMC";
+            this.btnMC.Size = new System.Drawing.Size(82, 64);
+            this.btnMC.TabIndex = 23;
+            this.btnMC.Text = "MC";
+            this.btnMC.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -340,25 +340,25 @@
             this.button3.Text = "M+";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnMS
             // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.button4.Location = new System.Drawing.Point(388, 428);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(82, 64);
-            this.button4.TabIndex = 26;
-            this.button4.Text = "MS";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnMS.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnMS.Location = new System.Drawing.Point(388, 428);
+            this.btnMS.Name = "btnMS";
+            this.btnMS.Size = new System.Drawing.Size(82, 64);
+            this.btnMS.TabIndex = 26;
+            this.btnMS.Text = "MS";
+            this.btnMS.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(482, 512);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnMS);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMC);
             this.Controls.Add(this.btnSqrt);
             this.Controls.Add(this.btn1Dividex);
             this.Controls.Add(this.lblDisplay);
@@ -414,10 +414,10 @@
         private System.Windows.Forms.Label lblDisplay;
         private System.Windows.Forms.Button btn1Dividex;
         private System.Windows.Forms.Button btnSqrt;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnMC;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnMS;
     }
 }
 
